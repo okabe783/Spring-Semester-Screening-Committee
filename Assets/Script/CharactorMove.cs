0d0a78630438c83f6acd8dd7a7487edce742e0d7
okabe783 <134473737+okabe783@users.noreply.github.com>
2024-01-06 18:28:17 +0900

    [Move]Animationで歩かせた

--- Assets/Script/CharactorMove.cs
+++ Assets/Script/CharactorMove.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class CharactorMove : MonoBehaviour
@@ -7,10 +6,12 @@ public class CharactorMove : MonoBehaviour
     
     private CharacterController characterController;
     private Vector3 velocity; //速度
+    private Animator animator;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,6 +21,16 @@ public class CharactorMove : MonoBehaviour
         {
             //xは左右方向、yは上下なので0、zは前後方向を取得
             velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            //magnitudeはベクトルの長さ
+            if (velocity.magnitude > 0.1f)
+            {
+                animator.SetFloat("Speed",velocity.magnitude);
+                transform.LookAt(transform.position + velocity);
+            }
+            else
+            {
+                animator.SetFloat("Speed", 0f);
+            }
         }
         velocity.y += Physics.gravity.y * Time.deltaTime;
         characterController.Move(velocity * Speed * Time.deltaTime);
